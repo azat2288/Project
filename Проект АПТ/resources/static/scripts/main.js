d73e0ab4af23237ea7edcb48f1d5dcc08413b178
azat2288 <119626796+azat2288@users.noreply.github.com>
2023-05-22 11:58:49 +0300

    Add files via upload

---
+++ Проект АПТ/resources/static/scripts/main.js
@@ -0,0 +1,172 @@
+window.addEventListener ('scroll', function() {
+
+    const scrollValue = window.pageYOffset;
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+
+    // nav P-----------------------------------------
+    if (scrollValue >= 0) {
+        this.document.getElementById('frst').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+        this.document.getElementById('zero').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+
+        this.document.getElementById('zerozero').style = ""
+        this.document.getElementById('thrd').style = ""
+        this.document.getElementById('frth').style = ""
+        this.document.getElementById('sec').style = ""
+        this.document.getElementById('fv').style = ""
+        this.document.getElementById('sx').style = ""
+    }
+
+    if (scrollValue >= 815) {
+        this.document.getElementById('sec').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+
+        this.document.getElementById('frth').style = ""
+        this.document.getElementById('frst').style = ""
+        this.document.getElementById('thrd').style = ""
+        this.document.getElementById('fv').style = ""
+        this.document.getElementById('sx').style = ""
+    }
+
+
+    if (scrollValue >= 1690) {
+        this.document.getElementById('thrd').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+        this.document.getElementById('zerozero').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+
+        this.document.getElementById('zero').style = ""
+        this.document.getElementById('sec').style = ""
+        this.document.getElementById('frth').style = ""
+        this.document.getElementById('frst').style = ""
+        this.document.getElementById('fv').style = ""
+        this.document.getElementById('sx').style = ""
+    }
+
+    if (scrollValue >= 2910) {
+        this.document.getElementById('frth').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+
+        this.document.getElementById('thrd').style = ""
+        this.document.getElementById('sec').style = ""
+        this.document.getElementById('frst').style = ""
+        this.document.getElementById('fv').style = ""
+        this.document.getElementById('sx').style = ""
+    }
+
+
+    if (scrollValue >= 3535) {
+        this.document.getElementById('fv').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+
+        this.document.getElementById('thrd').style = ""
+        this.document.getElementById('sec').style = ""
+        this.document.getElementById('frst').style = ""
+        this.document.getElementById('frth').style = ""
+        this.document.getElementById('sx').style = ""
+    }
+
+    
+    if (scrollValue >= 4200) {
+        this.document.getElementById('sx').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #4685b7, #4685b7, #4685b7, #4685b7, #ffdc63, #ffdc63, #ffdc63, #ffdc63); color: transparent"
+
+        this.document.getElementById('thrd').style = ""
+        this.document.getElementById('sec').style = ""
+        this.document.getElementById('frst').style = ""
+        this.document.getElementById('frth').style = ""
+        this.document.getElementById('fv').style = ""
+    }
+
+    // nav P-----------------------------------------
+
+})
+
+
+window.addEventListener ('scroll', function() {
+
+    const scrollValue = window.pageYOffset;
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+    
+    // nav JS-----------------------------------------
+
+    if (scrollValue >= 0) {
+        this.document.getElementById('1').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+        this.document.getElementById('0').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+
+        this.document.getElementById('00').style = ""
+        this.document.getElementById('2').style = ""
+        this.document.getElementById('3').style = ""
+        this.document.getElementById('4').style = ""
+        this.document.getElementById('5').style = ""
+        this.document.getElementById('6').style = ""
+        this.document.getElementById('7').style = ""
+    }
+
+    if (scrollValue >= 800) {
+        this.document.getElementById('2').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+
+        this.document.getElementById('1').style = ""
+        this.document.getElementById('3').style = ""
+        this.document.getElementById('4').style = ""
+        this.document.getElementById('5').style = ""
+        this.document.getElementById('6').style = ""
+        this.document.getElementById('7').style = ""
+    }
+
+
+    if (scrollValue >= 2020) {
+        this.document.getElementById('3').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+
+        this.document.getElementById('1').style = ""
+        this.document.getElementById('2').style = ""
+        this.document.getElementById('4').style = ""
+        this.document.getElementById('5').style = ""
+        this.document.getElementById('6').style = ""
+        this.document.getElementById('7').style = ""
+    }
+
+    if (scrollValue >= 2435) {
+        this.document.getElementById('4').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+        this.document.getElementById('00').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+
+        this.document.getElementById('0').style = ""
+        this.document.getElementById('1').style = ""
+        this.document.getElementById('2').style = ""
+        this.document.getElementById('3').style = ""
+        this.document.getElementById('5').style = ""
+        this.document.getElementById('6').style = ""
+        this.document.getElementById('7').style = ""
+    }
+
+
+    if (scrollValue >= 3750) {
+        this.document.getElementById('5').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+
+        this.document.getElementById('1').style = ""
+        this.document.getElementById('2').style = ""
+        this.document.getElementById('3').style = ""
+        this.document.getElementById('4').style = ""
+        this.document.getElementById('6').style = ""
+        this.document.getElementById('7').style = ""
+    }
+
+    
+    if (scrollValue >= 4200) {
+        this.document.getElementById('6').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+
+        this.document.getElementById('1').style = ""
+        this.document.getElementById('2').style = ""
+        this.document.getElementById('3').style = ""
+        this.document.getElementById('4').style = ""
+        this.document.getElementById('5').style = ""
+        this.document.getElementById('7').style = ""
+    }
+
+    if (scrollValue >= 5020) {
+        this.document.getElementById('7').style = "-webkit-background-clip: text; background-clip: text; background-image: linear-gradient(to right, #fffe00, #f6e600, #ebcf00, #dfb900, #d1a301); color: transparent"
+
+        this.document.getElementById('1').style = ""
+        this.document.getElementById('2').style = ""
+        this.document.getElementById('3').style = ""
+        this.document.getElementById('4').style = ""
+        this.document.getElementById('5').style = ""
+        this.document.getElementById('6').style = ""
+    }
+
+    // nav JS-----------------------------------------
+
+})
